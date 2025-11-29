d453bbfa565aed460c522da67e523ccc62f52da9
Raquel Fernandez <raquelfdezsantos@gmail.com>
2025-11-29 19:15:57 +0100

    Añadir favicons en layout y documentación de rutas públicas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,13 @@
 
     <title>{{ config('app.name', 'Staynest') }}</title>
 
+    <!-- Favicons -->
+    <link rel="icon" type="image/png" href="{{ asset('favicon-96x96.png') }}" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}" />
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}" />
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}" />
+
     <!-- Staynest Styles -->
     <link rel="stylesheet" href="{{ asset('css/staynest.css') }}">
 
